7a48729bcc57734a46a2ff593ccf814782ce3e91
jsadler2 <jeffrey.sadler2@gmail.com>
2018-03-05 16:17:33 -0500

    get upstream conduits method

--- scripts/get_contributing_area.py
+++ scripts/get_contributing_area.py
@@ -4,6 +4,11 @@ nodes = mymodel.nodes()
 cons = mymodel.conduits()
 subs = mymodel.subcatchments()
 
+def get_upstream_conduits(node_id, con_df):
+    us_nodes = get_upstream_nodes(node_id, con_df)
+    us_cons = con_df[(con_df.InletNode.isin(us_nodes)) | (con_df.OutletNode.isin(us_nodes))] 
+    return us_cons.index
+
 def get_upstream_nodes_one(node_id, con_df):
     conids = con_df[con_df["OutletNode"] == node_id].index
     if len(conids)>0:
@@ -12,25 +17,25 @@ def get_upstream_nodes_one(node_id, con_df):
     else:
         return []
 
-def get_upstream_nodes(node_id):
-    l = get_upstream_nodes_one(node_id, cons)
+def get_upstream_nodes(node_id, con_df):
+    l = get_upstream_nodes_one(node_id, con_df)
     for n in l:
-        l.extend(get_upstream_nodes_one(n, cons))
+        l.extend(get_upstream_nodes_one(n, con_df))
     return l
 
-def get_contributing_subs(node_id):
-    us_nodes = get_upstream_nodes(node_id)
-    us_subs = subs[subs["Outlet"].isin(us_nodes)]
+def get_contributing_subs(node_id, con_df, subs_df):
+    us_nodes = get_upstream_nodes(node_id, con_df)
+    us_subs = subs_df[subs["Outlet"].isin(us_nodes)]
     return us_subs
 
-def get_contributing_area(node_id):
-    cont_subs = get_contributing_subs(node_id)
+def get_contributing_area(node_id, con_df, subs_df):
+    cont_subs = get_contributing_subs(node_id, con_df, subs_df)
     return cont_subs["Area"].sum()
 
 
-a0 = get_contributing_area("St1")
-a1 = get_contributing_area("E143351")
-a2 = get_contributing_area("E144050")
-a3 = get_contributing_area("E146004")
+a0 = get_contributing_area("St1", cons, subs)
+a1 = get_contributing_area("E143351", cons, subs)
+a2 = get_contributing_area("E144050", cons, subs)
+a3 = get_contributing_area("E146004", cons, subs)
 
 
