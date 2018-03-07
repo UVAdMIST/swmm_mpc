d28dfda896fc8a634a2a0170a3a95c6804aa3916
jsadler2 <jeffrey.sadler2@gmail.com>
2018-03-07 15:01:43 -0500

    evaluate function in run_ea written

--- scripts/run_ea.py
+++ scripts/run_ea.py
@@ -1,25 +1,54 @@
 from deap import base, creator, tools, algorithms
+from swmmio import swmmio
 import random
+import os
+from shutil import copyfile
+import subprocess
+from update_process_model_input_file import update_controls
+from run_swmm_mpc import input_process_file_inp, input_process_file_base, control_time_step, \
+        control_str_id
 
-def chunks(l, n):
-    """Yield successive n-sized chunks from l."""
-    for i in xrange(0, len(l), n):
-        yield l[i:i + n]
-
-def bit_to_decimal(l):
-    d_str = "".join(str(i) for i in l)
-    return int(d_str, 2)
+FNULL = open(os.devnull, 'w')
 
 def evaluate(individual):
-    pass
+    # make process model tmp file
+    input_tmp_process_file_base = input_process_file_base + "_tmpi"
+    input_tmp_process_inp = input_tmp_process_file_base + ".inp"
+    copyfile(input_process_file_inp, input_tmp_process_inp)
+
+    # convert individual to percentages
+    indivi_percentage = [setting/10. for setting in individual]
+    policies = {control_str_id: indivi_percentage}
+
+    # update controls
+    update_controls(input_tmp_process_inp, control_time_step, policies)
+
+    # run the swmm model
+    cmd = "swmm5.exe {0}.inp {0}.rpt".format(input_tmp_process_file_base)
+    subprocess.call(cmd, shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
+
+    # read the output file
+    mymodel = swmmio.Model(input_tmp_process_inp)
+    nodes = mymodel.nodes()
+    nodes.fillna(0, inplace=True)
+    storage_flood_volume = nodes.loc['St1']['TotalFloodVol']
+    node_flood_volume = nodes.loc['J2']['TotalFloodVol']
+    target_storage_level = 1.
+    avg_dev_fr_tgt_st_lvl = nodes.loc['St1', 'AvgDepth'] - target_storage_level
+
+    # convert the contents of the output file into a cost
+    cost = 2 * storage_flood_volume**3 + node_flood_volume*2 + avg_dev_fr_tgt_st_lvl 
+    # os.remove(input_tmp_process_inp)
+    # os.remove(input_tmp_process_file_base + ".rpt")
+    return cost
 
 creator.create('FitnessMin', base.Fitness, weights=(-1.0,))
 creator.create('Individual', list, fitness=creator.FitnessMin)
 
 toolbox = base.Toolbox()
-toolbox.register("attr_bool", random.randint, 0, 1)
+toolbox.register("attr_int", random.randint, 0, 10)
 toolbox.register("individual", tools.initRepeat, creator.Individual,
-                 toolbox.attr_bool, 96)
+                 toolbox.attr_int, 24)
 toolbox.register('population', tools.initRepeat, list, toolbox.individual)
 toolbox.register('evaluate', evaluate)
 toolbox.register("mate", tools.cxTwoPoint)
