a89cee724bf52208078b52947740ef114e331c10
jsadler2 <jeffrey.sadler2@gmail.com>
2018-03-07 11:06:08 -0500

    separated out update functions from main mpc file

--- scripts/run_swmm_mpc.py
+++ scripts/run_swmm_mpc.py
@@ -1,9 +1,7 @@
-import re
-import os
-import subprocess
 import pyswmm
 from pyswmm import Simulation, Nodes, Links
 from shutil import copyfile
+from update_process_model_input_file import update_process_model_file
 import time
 pyswmm.lib.use("swmm5_conda.dll")
 
@@ -12,24 +10,6 @@ input_file_tmp_base = input_file.replace(".inp", "_tmp")
 input_file_tmp_inp = input_file_tmp_base + ".inp"
 copyfile(input_file, input_file_tmp_inp)
 
-def update_depths_or_flows(lines, i, depths_or_flows, col_name):
-    init_depth_loc = lines[i+1].find(col_name)
-    for obj_id in depths_or_flows:
-        for j, l in enumerate(lines[i:]):
-            if l.startswith(obj_id):
-                new_value_string = "{: <11.4f}".format(depths_or_flows[obj_id]) 
-                new_string = l[:init_depth_loc] + new_value_string + l[init_depth_loc+11: ]
-                lines[i+j] = new_string
-                break 
-    return lines
-
-def update_simulation_date_time(lines, i, new_datetime):
-    new_date = new_datetime.strftime("%m/%d/%Y")
-    new_time = new_datetime.strftime("%H:%M:%S")
-    lines[i] = re.sub(r'\d{2}\\\d{2}\\\d{2}', new_date, lines[i])
-    lines[i+1] = re.sub(r'\d{2}:\d{2}:\d{2}', new_time, lines[i+1])
-    return lines
-    
 
 def get_link_flows(link_obj):
     flow_dict = {}
@@ -52,36 +32,15 @@ def get_node_depths(node_obj):
             'outfalls': outfall_depth_dict }
     return depth_dict
 
-
-def update_tmp_file(new_date_time, node_obj, link_obj):
-    depths = get_node_depths(node_obj)
-    flows = get_link_flows(link_obj)
-    with open(input_file_tmp_inp, 'r') as tmp_file:
-        lines = tmp_file.readlines()
-
-    for i,l in enumerate(lines):
-        if l.startswith("START_DATE"):
-            new_lines = update_simulation_date_time(lines, i, new_date_time)
-        elif l.startswith("[JUNCTIONS]"):
-            new_lines  = update_depths_or_flows(lines, i, depths['junctions'], "InitDepth")
-        elif l.startswith("[STORAGE]"):
-            new_lines = update_depths_or_flows(lines, i, depths['storage'], "InitDepth")
-        elif l.startswith("[CONDUITS]"):
-            new_lines = update_depths_or_flows(lines, i, flows, "InitFlow")
-
-
-    with open(input_file_tmp_inp, 'w') as tmp_file:
-        tmp_file.writelines(lines)
-
 start = time.time()
 with Simulation(input_file) as sim:
     sim.step_advance(600)
     for step in sim:
         node_obj = Nodes(sim)
+        depths = get_node_depths(node_obj)
         link_obj = Links(sim)
+        flows = get_link_flows(link_obj)
         current_date_time = sim.current_time
-        update_tmp_file(current_date_time, node_obj, link_obj)
-        cmd = "swmm5.exe {0}.inp {0}.rpt {0}.out".format(input_file_tmp_base)
-        subprocess.call(cmd, shell=True)
+        update_process_model_file(input_file_tmp_inp, current_date_time, depths, flows)
 end = time.time()
 print (end - start)
