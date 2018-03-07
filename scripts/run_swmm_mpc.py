7edf14e08b60e5e7a3aed8e9135edc85ef684784
jsadler2 <jeffrey.sadler2@gmail.com>
2018-03-07 15:03:09 -0500

    updated to 'process' model

--- scripts/run_swmm_mpc.py
+++ scripts/run_swmm_mpc.py
@@ -6,11 +6,12 @@ import time
 pyswmm.lib.use("swmm5_conda.dll")
 
 input_file = "../simple_model/simple_smart_blank.inp"
-input_file_tmp_base = input_file.replace(".inp", "_tmp")
-input_file_tmp_inp = input_file_tmp_base + ".inp"
-copyfile(input_file, input_file_tmp_inp)
+input_process_file_base = input_file.replace(".inp", "_process")
+input_process_file_inp = input_process_file_base + ".inp"
+copyfile(input_file, input_process_file_inp)
 
-
+control_time_step = 600
+control_str_id = "ORIFICE R1"
 def get_link_flows(link_obj):
     flow_dict = {}
     for l in link_obj:
@@ -34,13 +35,13 @@ def get_node_depths(node_obj):
 
 start = time.time()
 with Simulation(input_file) as sim:
-    sim.step_advance(600)
+    sim.step_advance(control_time_step)
     for step in sim:
         node_obj = Nodes(sim)
         depths = get_node_depths(node_obj)
         link_obj = Links(sim)
         flows = get_link_flows(link_obj)
         current_date_time = sim.current_time
-        update_process_model_file(input_file_tmp_inp, current_date_time, depths, flows)
+        update_process_model_file(input_process_file_inp, current_date_time, depths, flows)
 end = time.time()
 print (end - start)
