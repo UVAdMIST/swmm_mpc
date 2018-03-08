f83a20593c230c42d61936a61255ad4933b3b18a
jsadler2 <jeffrey.sadler2@gmail.com>
2018-03-08 15:15:55 -0500

    run_mpc get nsteps remaining, save depths df

--- scripts/run_swmm_mpc.py
+++ scripts/run_swmm_mpc.py
@@ -1,7 +1,9 @@
 import pyswmm
 from pyswmm import Simulation, Nodes, Links
 from shutil import copyfile
-from update_process_model_input_file import update_process_model_file
+import pandas as pd
+import update_process_model_input_file as up
+import run_ea
 import time
 pyswmm.lib.use("swmm5_conda.dll")
 
@@ -10,13 +12,29 @@ input_process_file_base = input_file.replace(".inp", "_process")
 input_process_file_inp = input_process_file_base + ".inp"
 copyfile(input_file, input_process_file_inp)
 
-control_time_step = 900
+control_horizon = 6. # hr
+control_time_step = 900. # sec
+n_control_steps = control_time_step*3600/control_time_step
 control_str_id = "ORIFICE R1"
+
+def get_flat_depth_dict(depths):
+    no_outer = [depths[key] for key in depths]
+    depths_flat = {k:v for d in no_outer for k,v in d.items()}
+    return depths_flat
+
 def get_link_flows(link_obj):
     flow_dict = {}
     for l in link_obj:
         flow_dict[l.linkid] = l.flow
     return flow_dict
+
+def get_nsteps_remaining(sim):
+    time_remaining = sim.end_time - sim.current_time 
+    steps = time_remaining/control_time_step
+    if steps < n_control_steps:
+        return steps
+    else:
+        return n_control_steps
 
 def get_node_depths(node_obj):
     junction_depth_dict = {}
@@ -36,17 +54,36 @@ def get_node_depths(node_obj):
 
 def main():
     start = time.time()
+    depth_ts = []
     with Simulation(input_file) as sim:
         sim.step_advance(control_time_step)
         for step in sim:
+            # get most current system states
             node_obj = Nodes(sim)
             depths = get_node_depths(node_obj)
+            depth_ts.append(get_flat_depth_dict(depths))
             link_obj = Links(sim)
+            orifice = link_obj["R1"]
             flows = get_link_flows(link_obj)
             current_date_time = sim.current_time
-            update_process_model_file(input_process_file_inp, current_date_time, depths, flows)
+
+            # update the process model with the current states
+            up.update_process_model_file(input_process_file_inp, current_date_time, depths, flows)
+
+            # get num control steps remaining
+            nsteps = get_nsteps_remaining(sim)
+
+            # run prediction to get best policy 
+            best_policy = run_ea.run_ea(nsteps)
+
+            #implement best policy
+            orifice.target_setting = best_policy[0]/10.
+
             end = time.time()
-            print (end - start)
+    print (end - start)
+    df = pd.DataFrame(depth_ts)
+    df.to_csv("../data/depth_results.csv")
+    return df
 
 if __name__ == "__main__":
     main()
