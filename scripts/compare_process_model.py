9822f92ff71012747bc08eaf27ba4fc95aacd89b
jsadler2 <jeffrey.sadler2@gmail.com>
2018-04-04 10:58:57 -0400

    more logical var names compare_process_model

--- scripts/compare_process_model.py
+++ scripts/compare_process_model.py
@@ -1,19 +1,19 @@
 from plot_rpt import rpt_ele
 import matplotlib.pyplot as plt
 
-rpt_file = ("../simple_model/s_s_b_630.rpt")
-rpt_file2 = ("../simple_model/s_s_b.rpt")
-rpt_file3 = ("../simple_model/s_s_b_500.rpt")
+rpt_file = ("../simple_model/s_s_b.rpt")
 rpt_file4 = ("../simple_model/s_s_b_400.rpt")
+rpt_file4 = ("../simple_model/s_s_b_500.rpt")
+rpt_file630 = ("../simple_model/s_s_b_630.rpt")
 rpt = rpt_ele(rpt_file, "Node J1", "Inflow")
-rpt2 = rpt_ele(rpt_file2, "Node J1", "Inflow")
-rpt3 = rpt_ele(rpt_file3, "Node J1", "Inflow")
 rpt4 = rpt_ele(rpt_file4, "Node J1", "Inflow")
+rpt5 = rpt_ele(rpt_file5, "Node J1", "Inflow")
+rpt630 = rpt_ele(rpt_file630, "Node J1", "Inflow")
 
-ax = rpt2.ele_df["Inflow"].plot(label="Reality model")
-rpt.ele_df["Inflow"].plot(ax=ax, label="Process start 06:30")
-rpt3.ele_df["Inflow"].plot(ax=ax, label="Process start 05:00")
+ax = rpt.ele_df["Inflow"].plot(label="Reality model")
 rpt4.ele_df["Inflow"].plot(ax=ax, label="Process start 04:00")
+rpt5.ele_df["Inflow"].plot(ax=ax, label="Process start 05:00")
+rpt630.ele_df["Inflow"].plot(ax=ax, label="Process start 06:30")
 ax.legend()
 ax.set_ylabel("Flow (cfs)")
 ax.set_xlabel("Date/Time")
