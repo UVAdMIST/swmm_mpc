9a582da2ba51eadf8e842900a8924706583a694c
jsadler2 <jeffrey.sadler2@gmail.com>
2018-04-18 15:30:30 -0400

    removed unnessecarry files from scripts/

--- scripts/initialize_pyswmm.py
+++ scripts/initialize_pyswmm.py
@@ -4,4 +4,12 @@ import pyswmm
 pyswmm.lib.use("/home/jeff/Documents/research/Sadler4th_paper/_build/lib/libswmm5.so")
 
 inp_file = "../simple_model/simple_smart_blank.inp"
-sim = Simulation(inp_file)
+# sim = Simulation(inp_file)
+with Simulation(inp_file) as sim:
+    i = 0
+    for step in sim:
+        print step
+        i += 1
+        if i == 5:
+            sim._model.hotstart_open("{}.hsf".format(i))
+
