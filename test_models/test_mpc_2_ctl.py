59789368a863b73f0e1bf5ef795fb6d161679c91
jsadler2 <jeffrey.sadler2@gmail.com>
2018-06-11 11:09:16 -0400

    test 2 ctl x refactored swmm_mpc

--- test_models/test_mpc_2_ctl.py
+++ test_models/test_mpc_2_ctl.py
@@ -1,4 +1,5 @@
-from swmm_mpc.swmm_mpc import swmm_mpc
+from swmm_mpc.swmm_mpc import run_swmm_mpc
+
 inp_file = "simple_2_ctl.inp"
 control_horizon = 3. #hr
 control_time_step = 900. #sec
@@ -9,18 +10,18 @@ nindividuals = 50
 
 # target_depth_dict={'St1':{'target':1, 'weight':0.1}, 'St2':{'target':1.5, 'weight':0.1}}
 
-swmm_mpc_obj = swmm_mpc(inp_file,
-			control_horizon,
-			control_time_step,
-			control_str_ids,
-			results_dir,
-                        # target_depth_dict=target_depth_dict,
-                        ngen=ngen,
-                        nindividuals=nindividuals
-                        )
+
 
 def main():
-    swmm_mpc_obj.run_swmm_mpc()
+    run_swmm_mpc(inp_file,
+                 control_horizon,
+                 control_time_step,
+                 control_str_ids,
+                 results_dir,
+                 # target_depth_dict=target_depth_dict,
+                 ngen=ngen,
+                 nindividuals=nindividuals
+                 )
 
 if __name__ == "__main__":
     main()
