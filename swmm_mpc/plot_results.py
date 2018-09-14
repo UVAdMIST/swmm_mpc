00abf336e79ae39874fa96905327529325adc770
jsadler2 <jeffrey.sadler2@gmail.com>
2018-09-14 13:43:28 -0400

    updated, enhanced plot_results

--- swmm_mpc/plot_results.py
+++ swmm_mpc/plot_results.py
@@ -1,50 +1,96 @@
-from rpt_ele import rpt_ele
-from update_process_model_input_file import update_controls_with_policy
-import sys
-import matplotlib.pyplot as plt
-import subprocess
 import os
+import subprocess
+import matplotlib.pyplot as plt
+from matplotlib import rc
+from swmm_mpc.rpt_ele import rpt_ele
+from swmm_mpc.update_process_model_input_file import update_controls_with_policy
+import pandas as pd
 
-inp_file = sys.argv[1]
-policy_file = sys.argv[2]
-control_time_step = float(sys.argv[3])
+def get_df(rpts, ele, variable, column_names=None):
+    """
+    get pandas dataframe of different versions of results for one element for
+    one variable
+    rpts: list of rpt_ele objects - rpt_objects to be combined
+    ele: string - swmm model element to extract from rpt_ele objects 
+         (e.g., "Node J3")
+    variable: string - variable to extract data (e.g., "Depth")
+    column_names: list of strings - column labels for different versions
+                  (e.g., ["Passive", "Rules", "MPC"])
+    """
+    ser_list = []
+    for rpt in rpts:
+        ser_list.append(rpt.get_ele_df(ele)[variable])
+    comb = pd.concat(ser_list, 1)
+    if column_names:
+        comb.columns=column_names
+    return comb
 
-policy_id = policy_file.split("/")[-1].split("_")[-1]
+def plot_versions_single(df, ylabel, title=None, colors=None):
+    """
+    make a plot of multiple versions of rpt_elements at one node for one 
+    variable
+    df: pandas dataframe - dataframe with values of one variable at one node 
+        for different versions. The different versions are the columns
+    ylabel: string - label for y-axis
+    title: string - title for plot
+    colors: list of strings - matplotlib colors corresponding to the different
+            versions of the results
+    """
+    font_size = 12
+    ax = df.plot(fontsize=font_size, lw=3)
+    lines = ax.lines
 
-update_controls_with_policy(inp_file, control_time_step, policy_file)
+    if not colors:
+        colors = ["0.55", "royalblue", "yellowgreen"]
 
-# run swmm
-rpt_file = inp_file.replace(".inp", ".rpt") 
-cmd = "swmm5 {} {}".format(inp_file, rpt_file)
-subprocess.call(cmd, shell=True, stderr=subprocess.STDOUT)
+    for i in range(len(lines)):
+        lines[i].set_color(colors[i])
 
-rpt_obj = rpt_ele(rpt_file)
-variable = 'Depth'
-ax = rpt_obj.get_ele_df('NODE J3')[variable].plot(label=variable+" (ft)")
-variable = 'Flooding'
-ax = rpt_obj.get_ele_df('NODE J3')[variable].plot(label=variable+" (million gallons)")
-ax.set_title("NODE J3 results from {}".format(policy_id))
-ax.legend()
-plt.tight_layout()
-plt.show()
+    ax.legend()
+    ax.set_xlabel("Time elapsed (hr)", fontsize=font_size)
+    ax.set_ylabel(ylabel, fontsize=font_size)
+    if title:
+        ax.set_title(title)
+    return ax
 
-variable = 'Depth'
-ax = rpt_obj.get_ele_df('NODE J2')[variable].plot(label=variable+" (ft)")
-variable = 'Flooding'
-ax = rpt_obj.get_ele_df('NODE J2')[variable].plot(label=variable+" (million gallons)")
-ax.set_title("NODE J3 results from {}".format(policy_id))
-ax.legend()
-plt.tight_layout()
-plt.show()
+def plot_versions_together(node_id_vars, rpt_files, rpt_labels, units, fig_dir,
+                           sfx):
+    """
+    plot variable results at different nodes in one figure
+    node_id_vars: list of tuples - tuple has node_id as first element and 
+                  variable to plot as second element 
+                  (e.g., [("Node St1", "Depth"), ("Node J3", "Flooding")])
+    rpts_files: list of strings - file paths of different versions that will be 
+                in each sublot 
+    rpt_labels: list of strings - labels for the different rpt versions
+                (e.g., ["Passive", "Rules", "MPC"])
+    units: string - units of variable to be plotted
+    fig_dir: string - directory where file should be saved
+    sfx: string - suffix to be put on the end of the file name
+    """
+    rpts = [rpt_ele(r) for r in rpt_files]
 
-variable = 'Depth'
-ax = rpt_obj.get_ele_df('NODE ST1')[variable].plot(label=variable+" (ft)")
-ax.set_title("NODE ST1 results from {}".format(policy_id))
-plt.tight_layout()
-plt.show()
+    nplots = len(rpts)
+    nrows = round(nplots**0.5)
+    fig, axs = plt.subplots(nrows=nrows, ncols=nrows, sharex=True, 
+                            figsize=(5,5))
 
-variable = 'Depth'
-ax = rpt_obj.get_ele_df('NODE ST2')[variable].plot(label=variable+" (ft)")
-ax.set_title("NODE ST2 results from {}".format(policy_id))
-plt.tight_layout()
-plt.show()
+    if nplots > 0:
+        axs_list = axs.ravel()
+    else:
+        axs_list = [axs]
+
+    counter = 0
+    for node_id, variable in node_id_vars:
+        var_df = get_df(rpts, node_id, variable, rpt_labels)
+        plot_title = "{} at {}".format(node_id, variable)
+        axs_list[counter] = plot_versions_single(var_df, title=plot_title)
+        counter += 1
+
+    plt.tight_layout()
+    fig = plt.gcf()
+    fig.savefig("../../figures/{}_{}".format(title.replace(" ", "_"),
+                                            sfx),  
+            bbox_inches="tight")
+    plt.show()
+
