f32f648b014abda5a05b9e598bd1ee565a02b749
jsadler2 <jeffrey.sadler2@gmail.com>
2018-10-11 13:43:56 -0400

    initial commit install.sh

---
+++ install.sh
@@ -0,0 +1,30 @@
+if ! [ -x "$(command -v unzip)" ]; then
+	sudo apt install unzip
+fi
+
+source activate swmm-mpc-py
+
+git clone https://github.com/UVAdMIST/swmm_mpc.git
+pip install swmm_mpc/
+
+pip install git+git://github.com/uva-hydroinformatics/pyswmm.git@feature_save_hotstart
+
+wget https://www.epa.gov/sites/production/files/2017-03/swmm51012_engine_2.zip
+unzip swmm51012_engine_2.zip -d swmm5
+cd swmm5/
+unzip source5_1_012.zip -d src
+unzip makefiles.zip -d mk
+cd mk
+unzip GNU-CLE.zip -d gnu
+cp gnu/Makefile ../src/
+cd ../src
+
+sed -i -e 's/#define DLL/\\\\#define DLL/g' swmm5.c
+sed -i -e 's/\\\\#define CLE/#define CLE/g' swmm5.c
+
+if ! [ -x "$(command -v make)" ]; then
+	sudo apt install build-essential
+fi
+
+export PATH="/path/to/swmm5/src:$PATH"
+echo 'PATH="/path/to/swmm5/src:$PATH"' >> bashrc_test
