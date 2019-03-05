43ea9ab742d9690b0a9fd1cfe915cc3021fce758
jsadler2 <jeffrey.sadler2@gmail.com>
2019-03-05 13:58:57 -0500

    mostly tabs/spacing, one critical bug in mutate

--- swmm_mpc/run_ea.py
+++ swmm_mpc/run_ea.py
@@ -10,12 +10,12 @@ import evaluate as ev
 creator.create('FitnessMin', base.Fitness, weights=(-1.0,))
 creator.create('Individual', list, fitness=creator.FitnessMin)
 
-pool = multiprocessing.Pool(16)
+pool = multiprocessing.Pool(8)
 toolbox = base.Toolbox()
 toolbox.register('map', pool.map)
 toolbox.register('attr_binary', random.randint, 0, 1)
 toolbox.register('mate', tools.cxTwoPoint)
-toolbox.register('mutate', tools.mutUniformInt, low=0, up=10, indpb=0.10)
+toolbox.register('mutate', tools.mutFlipBit, indpb=0.20)
 toolbox.register('select', tools.selTournament, tournsize=6)
 
 
@@ -27,7 +27,7 @@ def run_ea(ngen, nindividuals, work_dir, hs_file_path,
                      ev.evaluate,
                      hs_file_path=hs_file_path,
                      process_file_path=inp_process_file_path,
-		     sim_dt=sim_dt,
+                     sim_dt=sim_dt,
                      control_time_step=control_time_step,
                      n_control_steps=n_control_steps,
                      control_str_ids=control_str_ids,
@@ -35,7 +35,7 @@ def run_ea(ngen, nindividuals, work_dir, hs_file_path,
                      target_depth_dict=target_depth_dict,
                      flood_weight=flood_weight,
                      dev_weight=dev_weight
-                    )
+                     )
     policy_len = get_policy_length(control_str_ids, n_control_steps)
     toolbox.register('individual', tools.initRepeat, creator.Individual,
                      toolbox.attr_binary, policy_len)
@@ -44,7 +44,7 @@ def run_ea(ngen, nindividuals, work_dir, hs_file_path,
     # (not first time)
     pop_file = "{}population.json".format(work_dir)
     if os.path.isfile(pop_file):
-        toolbox.register("pop_guess", init_population, creator.Individual, 
+        toolbox.register("pop_guess", init_population, creator.Individual,
                          pop_file)
         pop = toolbox.pop_guess()
     else:
@@ -60,7 +60,7 @@ def run_ea(ngen, nindividuals, work_dir, hs_file_path,
     pop, logbook = algorithms.eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.2,
                                        ngen=ngen, stats=stats, halloffame=hof,
                                        verbose=True)
-    seed_next_population(hof[0], nindividuals, control_str_ids, pop_file, 
+    seed_next_population(hof[0], nindividuals, control_str_ids, pop_file,
                          n_control_steps)
     return hof[0]
 
@@ -70,22 +70,22 @@ def write_pop_to_file(population, pop_file):
     write a population of individuals to json file
     """
     with open(pop_file, 'w') as myfile:
-        json.dump(population, myfile) 
+        json.dump(population, myfile)
 
 
 def mutate_pop(best_policy, nindividuals, control_str_ids, n_steps):
     """
-    mutate the best policy of the current time step 
+    mutate the best policy of the current time step
     """
     list_of_inds = []
     for i in range(nindividuals):
-	# split because there may be more than one control
-	split_lists = ev.split_gene_by_ctl_ts(best_policy, control_str_ids,
+        # split because there may be more than one control
+        split_lists = ev.split_gene_by_ctl_ts(best_policy, control_str_ids,
                                               n_steps)
-	mutated_ind = []
-	for seg_by_ctl in split_lists:
+        mutated_ind = []
+        for seg_by_ctl in split_lists:
             # disregard the first control step since we need future policy
-	    seg_by_ctl = seg_by_ctl[1:]
+            seg_by_ctl = seg_by_ctl[1:]
             # mutate the remaining settings
             mutated_ctl_segment = []
             for seg_by_ts in seg_by_ctl:
@@ -95,34 +95,35 @@ def mutate_pop(best_policy, nindividuals, control_str_ids, n_steps):
             rand_sttng = [random.randint(0, 1) for i in range(len(seg_by_ts))]
             mutated_ctl_segment.extend(rand_sttng)
             # add the new policy for the control structure to the overall pol
-	    mutated_ind.extend(seg_by_ctl)
+            mutated_ind.extend(mutated_ctl_segment)
         # don't add the new indivi to the pop if identical indivi already there
         if mutated_ind not in list_of_inds:
             list_of_inds.append(mutated_ind)
     return list_of_inds
 
 
-def seed_next_population(best_policy, nindividuals, control_str_ids, pop_file
+def seed_next_population(best_policy, nindividuals, control_str_ids, pop_file,
                          n_steps):
     """
     seed the population for the next time step using the best policy from the
-    current time step as the basis. The policy 
+    current time step as the basis.
     best_policy:    [list] binary string representing best policy of current ts
     nindividuals:   [int]  number of individuals per generation in GA
     control_str_ids:[list] list of control ids (e.g., ['ORIFICE r1', ...])
     pop_file:       [string] name of file where the next seed pop will be saved
     n_steps:        [int] number of control time steps
-    return:         [list] mutated population 
+    return:         [list] mutated population
 
     """
-    mutated_pop = mutate_pop(best_policy, nindividuals, control_str_ids)
+    mutated_pop = mutate_pop(best_policy, nindividuals, control_str_ids,
+                             n_steps)
 
-    # in case there were duplicates after mutating, 
-    # fill the rest of the population with random individuals 
+    # in case there were duplicates after mutating,
+    # fill the rest of the population with random individuals
     while len(mutated_pop) < nindividuals:
         rand_ind = []
         for i in range(len(best_policy)):
-            rand_ind.append(random.randint(0, 10))
+            rand_ind.append(random.randint(0, 1))
         if rand_ind not in mutated_pop:
             mutated_pop.append(rand_ind)
     write_pop_to_file(mutated_pop, pop_file)
@@ -144,7 +145,7 @@ def init_population(ind_init, filename):
 
 def get_policy_length(control_str_ids, n_control_steps):
     """
-    get the length of the policy. ASSUMPTION - PUMP controls are binary 1 BIT, 
+    get the length of the policy. ASSUMPTION - PUMP controls are binary 1 BIT,
     ORIFICE and WEIR are 3 BITS
     returns:    [int] the number of total control decisions in the policy
     """
