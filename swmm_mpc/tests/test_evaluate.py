236a3cdc1224f27b552cf500d8fe716394e6b138
jsadler2 <jeffrey.sadler2@gmail.com>
2019-03-18 15:58:58 -0400

    test for split_gene_by_ctl_str_ids

--- swmm_mpc/tests/test_evaluate.py
+++ swmm_mpc/tests/test_evaluate.py
@@ -6,6 +6,7 @@ from swmm_mpc.rpt_ele import rpt_ele
 class test_evaluate(unittest.TestCase):
     rpt_file = "example.rpt"
     rpt = rpt_ele(rpt_file)
+    ctl_str_ids = ["ORIFICE r1", "PUMP p1"]
 
     def test_get_flood_cost_no_dict(self):
         node_fld_wgt_dict = None
@@ -20,8 +21,8 @@ class test_evaluate(unittest.TestCase):
     def test_gene_to_policy_dict(self):
         gene = [1, 0, 1, 1, 1, 0, 0, 1]
         n_ctl_steps = 2
-        ctl_str_ids = ["ORIFICE r1", "PUMP p1"]
-        policy = evaluate.gene_to_policy_dict(gene, ctl_str_ids, n_ctl_steps)
+        policy = evaluate.gene_to_policy_dict(gene, self.ctl_str_ids,
+                                              n_ctl_steps)
         # print (policy)
         self.assertEqual(policy, {'ORIFICE r1': [0.714, 0.857],
                                   'PUMP p1': ['OFF', 'ON']})
@@ -44,11 +45,18 @@ class test_evaluate(unittest.TestCase):
     def test_list_to_policy(self):
         gene = [0.4, 0.2, 0.1, 0.6, 0.2, 0]
         n_ctl_steps = 3
-        ctl_str_ids = ["ORIFICE r1", "PUMP p1"]
-        policy = evaluate.list_to_policy(gene, ctl_str_ids, n_ctl_steps)
-        # print (policy)
+        policy = evaluate.list_to_policy(gene, self.ctl_str_ids, n_ctl_steps)
         self.assertEqual(policy, {'ORIFICE r1': [0.4, 0.2, 0.1],
                                   'PUMP p1': ['ON', 'OFF', 'OFF']})
+
+    def test_split_gene_by_ctl_ts(self):
+        ctl_str_ids = ["ORIFICE r1", "ORIFICE p1"]
+        gene = [1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1]
+        n_steps = 2
+        split = evaluate.split_gene_by_ctl_ts(gene, ctl_str_ids, n_steps)
+        expected = [[[1, 0, 1], [0, 0, 1]], [[1, 0, 0], [1, 1, 1]]]
+        self.assertEqual(expected, split)
+        
 
 
 if __name__ == '__main__':
