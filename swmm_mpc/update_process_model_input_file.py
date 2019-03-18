5a9d3f510faf4d8ba68fa7c8bc95d3b87ec5e81e
jsadler2 <jeffrey.sadler2@gmail.com>
2019-03-18 15:59:49 -0400

    some flexibility in finding time step

--- swmm_mpc/update_process_model_input_file.py
+++ swmm_mpc/update_process_model_input_file.py
@@ -170,7 +170,11 @@ def get_control_time_step(df, dt_col="datetime"):
     times = (pd.to_datetime(df[dt_col]))
     delta_times = times.diff()
     time_step = delta_times.mean().seconds
-    if time_step % 60:
-        raise Exception("The time step in your file is in between minutes")
-    else:
+    if not time_step % 60:
         return time_step
+    # if it's only off by 1 or two seconds then round down to nearest minute
+    elif time_step % 60 < 3:
+        time_step -= time_step % 60
+        return time_step 
+    else:
+        raise Exception("The time step in your file is in between minutes")
