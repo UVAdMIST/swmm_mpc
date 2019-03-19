fa052a120a39e3e228e3c11f146e26bcd3485feb
jsadler2 <jeffrey.sadler2@gmail.com>
2019-03-19 13:09:08 -0400

    added initial guesses, support multiple timesteps

--- swmm_mpc/run_baeopt.py
+++ swmm_mpc/run_baeopt.py
@@ -1,32 +1,42 @@
 import GPyOpt
 import swmm_mpc as sm
 import evaluate as ev
+import numpy as np
 from GPyOpt.methods import BayesianOptimization as BayOpt
 
 
-def get_bounds(ctl_str_ids):
+def get_bounds(ctl_str_ids, nsteps):
     bounds = []
-    for i, ctl in enumerate(ctl_str_ids):
-        ctl_type = ctl.split()[0]
-        ctl_bounds = {}
+    for ctl in ctl_str_ids:
+        var_num = 0
+        for j in range(nsteps):
+            ctl_type = ctl.split()[0]
+            ctl_bounds = {}
 
-        if ctl_type == 'WEIR' or ctl_type == 'ORIFICE':
-            var_type = 'continuous'
-        elif ctl_type == 'PUMP':
-            var_type = 'discrete'
+            if ctl_type == 'WEIR' or ctl_type == 'ORIFICE':
+                var_type = 'continuous'
+            elif ctl_type == 'PUMP':
+                var_type = 'discrete'
 
-        ctl_bounds['name'] = 'var_{}'.format(i)
-        ctl_bounds['type'] = var_type
-        ctl_bounds['domain'] = (0, 1)
-        bounds.append(ctl_bounds)
+            ctl_bounds['name'] = 'var_{}'.format(var_num)
+            ctl_bounds['type'] = var_type
+            ctl_bounds['domain'] = (0, 1)
+            var_num += 1
+            bounds.append(ctl_bounds)
     return bounds
 
 
 def run_baeopt(opt_params):
     # set up opt params
-    bounds = get_bounds(sm.run.ctl_str_ids)
+    bounds = get_bounds(sm.run.ctl_str_ids, sm.run.n_ctl_steps)
     max_iter = opt_params.get('max_iter', 15)
     max_time = opt_params.get('max_time', 120)
+    initial_guess = opt_params.get('initial_guess', [])
+    if len(initial_guess) == 0:
+        initial_guess = None
+    else:
+        initial_guess = np.array([initial_guess])
+
     eps = opt_params.get('eps', 0.01)
     model_type = opt_params.get('model_type', 'GP')
     acquisition_type = opt_params.get('acquisition_type', 'EI')
@@ -36,6 +46,7 @@ def run_baeopt(opt_params):
                      domain=bounds,
                      model_type=model_type,
                      acquisition_type='EI',
+                     X=initial_guess,
                      evaluator_type='local_penalization',
                      num_cores=opt_params['num_cores'],
                      batch_size=opt_params['num_cores'],
--- swmm_mpc/swmm_mpc.py
+++ swmm_mpc/swmm_mpc.py
@@ -146,6 +146,7 @@ def run_swmm_mpc(config_file):
                     '{} not valid opt method'.format(run.opt_method)
                     )
             print best_policy, cost
+            run.optimization_params['initial_guess'] = best_policy
 
             best_policy_fmt = ev.format_policies(best_policy,
                                                  run.ctl_str_ids,
